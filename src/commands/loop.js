07ff96079dcbad11220f5f6e55ab3df2415b2d5b
Rioni <nrosa01@ucm.es>
2022-10-23 15:55:03 +0200

    loop feature

---
+++ src/commands/loop.js
@@ -0,0 +1,36 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import pkg from "discord.js"
+const { EmbedBuilder } = pkg
+
+export default {
+    data: new SlashCommandBuilder()
+    .setName("loop")
+    .setDescription("Loops the current song"),
+
+    execute: async ({ client, interaction }) => {
+            
+            // Get the queue for the server
+            const queue = client.player.getQueue(interaction.guildId)
+    
+            // If there is no queue, return
+            if (!queue)
+            {
+                await interaction.reply("There are no songs in the queue");
+                return;
+            }
+    
+            const currentSong = queue.current
+    
+            // Skip the current song
+            queue.setRepeatMode(1)
+    
+            // Return an embed to the user saying the song has been skipped
+            await interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                    .setDescription(`${currentSong.title} has been looped!`)
+                    .setThumbnail(currentSong.thumbnail)
+                ]
+            })
+        }
+}
